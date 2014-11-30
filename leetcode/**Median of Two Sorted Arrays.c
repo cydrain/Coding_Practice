e6a5fa889a90abc8db3584d21fc64ecad91af8af
Cai Yudong <cydrain@gmail.com>
2014-11-30 23:25:09 +0800

    update 20141130

--- leetcode/**Median of Two Sorted Arrays.c
+++ leetcode/**Median of Two Sorted Arrays.c
@@ -7,6 +7,50 @@
  *
  */
 
+// solution I, similar as solution II, but avoid calling findKthElemtent() twice
+    double findKthElement(int A[], int m, int B[], int n, int k, bool isEven) {
+        int Ai_1, Ai, Aiplus1, Bj_1, Bj, Bjplus1;
+        int i, j;
+        
+        i = k * m/(m+n);
+        j = k - i;
+        Ai_1 = (i == 0) ? INT_MIN : A[i-1];
+        Bj_1 = (j == 0) ? INT_MIN : B[j-1];
+        Ai   = (i == m) ? INT_MAX : A[i];
+        Bj   = (j == n) ? INT_MAX : B[j];
+        
+        if (Ai_1 <= Bj && Bj <= Ai) {
+            if (isEven) {
+                Bjplus1 = ((j >= n-1) ? INT_MAX : B[j+1]);
+                return (double)(Bj + min(Ai, Bjplus1))/2.0;
+            }
+            else {
+                return Bj;
+            }
+        }
+        if (Bj_1 <= Ai && Ai <= Bj) {
+            if (isEven) {
+                Aiplus1 = ((i >= m-1) ? INT_MAX : A[i+1]);
+                return (double)(Ai + min(Aiplus1, Bj))/2.0;
+            }
+            else {
+                return Ai;
+            }
+        }
+        
+        if (Ai <= Bj) return findKthElement(A+i+1, m-i-1, B, n, k-i-1, isEven);
+        if (Bj <= Ai) return findKthElement(A, m, B+j+1, n-j-1, k-j-1, isEven);
+    }
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+        if ((m+n) % 2) {
+            return findKthElement(A, m, B, n, (m+n-1)/2, false);
+        }
+        else {
+            return findKthElement(A, m, B, n, (m+n-1)/2, true);
+        }
+    }
+
+// solution II
     // find the No.k element, (1 <= k <= m+n)
     // i + j == k - 1
     int findKthSmall(int A[], int m, int B[], int n, int k) {
