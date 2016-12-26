80b08e9a1a669fa39821089abc8b11cbc44f1c61
Cai Yudong <cydrain@gmail.com>
2016-12-26 20:40:57 +0800

    Update and rename Reverse Words in a String.c to 151-Reverse Words in a String.c

--- leetcode/151-Reverse Words in a String.c
+++ leetcode/151-Reverse Words in a String.c
@@ -6,6 +6,31 @@
 // Given s = "the sky is blue",
 // return "blue is sky the".
 //
+class Solution {
+public:
+    void reverseWords(string &s, int start, int end) {
+        while (start < end) {
+            swap(s[start], s[end]);
+            start++, end--;
+        }
+    }
+    void reverseWords(string &s) {
+        int len = s.length();
+        int i = 0, j = 0, p = 0;
+        reverseWords(s, 0, len-1);
+        for (i = 0; i < len; i++) {
+            if (s[i] != ' ') {
+                if (p != 0) s[p++] = ' ';
+                j = i;
+                while (j < len && s[j] != ' ') s[p++] = s[j++];
+                reverseWords(s, p-j+i, p-1);
+                i = j;
+            }
+        }
+        s.erase(s.begin()+p, s.end());
+    }
+};
+
     void reverseWords(string &s) {
         stack<string> sk;
         int start = -1, len = 0;
